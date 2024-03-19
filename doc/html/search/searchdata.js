93ba6b7eb85633d54773c7b7fcb63b7b6293e063
HudsonSchumaker <hudson.schumaker@me.com>
2024-03-19 20:42:50 +0100

    DE-NS-macOS: update doc

--- doc/html/search/searchdata.js
+++ doc/html/search/searchdata.js
@@ -1,18 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "dep",
-  1: "dep"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Pages"
 };
 
