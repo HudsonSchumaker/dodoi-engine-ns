8fda56b251ddcf30919b63be03b80a0b82b4161b
HudsonSchumaker <hudson.schumaker@me.com>
2024-03-21 20:11:16 +0100

    DE-NS-macOS-Config: update documentation.

--- doc/html/search/all_2.js
+++ doc/html/search/all_2.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['entity_0',['Entity',['../class_entity.html',1,'']]],
-  ['entity_2eh_1',['Entity.h',['../_entity_8h.html',1,'']]]
+  ['entity_2eh_1',['Entity.h',['../_entity_8h.html',1,'']]],
+  ['entitycomparator_2',['EntityComparator',['../struct_entity_comparator.html',1,'']]],
+  ['entitymanager_3',['EntityManager',['../class_entity_manager.html',1,'']]]
 ];
--- doc/html/search/classes_2.js
+++ doc/html/search/classes_2.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['entity_0',['Entity',['../class_entity.html',1,'']]]
+  ['entity_0',['Entity',['../class_entity.html',1,'']]],
+  ['entitycomparator_1',['EntityComparator',['../struct_entity_comparator.html',1,'']]],
+  ['entitymanager_2',['EntityManager',['../class_entity_manager.html',1,'']]]
 ];
