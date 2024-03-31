65d2802f17307563de85cf673be9ef64cc958103
HudsonSchumaker <hudson.schumaker@me.com>
2024-04-01 01:46:25 +0200

    DE-NS-macOS: update AudioManager.

--- src/engine/sfx/AudioManager.cpp
+++ src/engine/sfx/AudioManager.cpp
@@ -1,7 +1,28 @@
+/**
+* @file AudioManager.cpp
+* @author Hudson Schumaker
+* @brief Implements the AudioManager class.
+* @version 1.0.0
+* 
+* Dodoi-Engine-NS is a game engine developed by Dodoi-Lab.
+* @copyright Copyright (c) 2024, Dodoi-Lab
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*     http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 #include "AudioManager.h"
+#include "../io/FileUtils.h"
 
 AudioManager::AudioManager() {}
-
 AudioManager::~AudioManager() {
 	clearAssets();
 }
@@ -42,11 +63,10 @@ void AudioManager::unPauseSound(int channel) {
 	Mix_Resume(channel);
 }
 
-void AudioManager::playMusic(const std::string& filePath) {
-    std::async(std::launch::async, [&]() {
-        Mix_Music* music = Mix_LoadMUS(filePath.c_str());
+void AudioManager::playMusic(const std::string& filePath, int loop) {
+    auto res = std::async(std::launch::async, [&]() {
+        music = Mix_LoadMUS(filePath.c_str());
         Mix_PlayMusic(music, 0);
-        Mix_FreeMusic(music);
     });
 }
 
@@ -83,5 +103,5 @@ void AudioManager::clearAssets() {
 		Mix_FreeChunk(sound.second);
 	}
 	sounds.clear();
+	Mix_FreeMusic(music);
 }
-
